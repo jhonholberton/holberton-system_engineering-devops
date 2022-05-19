3892301382647e9d6a702c2679144c5f8689c094
jhonholberton <3425@holbertonschool.com>
2022-05-19 18:25:26 -0500

    Using what you did in the task #0, extend your Python script to export data in the CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,16 +2,10 @@
 import csv
 import requests
 from sys import argv
-"""
-accessing a url with employee ID to return information
-"""
-
+"""export data in the CSV format."""
 
 if __name__ == "__main__":
-    """
-    function to export the data into
-    csv file
-    """
+
     ID = int(argv[1])
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
                         format(ID)).json()
